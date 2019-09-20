af172b8fdd76f9e8f88be3347068583eb18d59e3
wszmuler <walter_szmuler@hotmail.com>
2019-09-20 23:37:40 +0000

    Adding The Excuse Generator

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,31 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = function myExcuse() {
+  let excuse = document.getElementById("excuse");
+  excuse.innerHTML = sentence();
+  let button = document.querySelector("#generateExcuse");
+  button.addEventListener("click", function() {
+    excuse.innerHTML = sentence();
+  });
 };
+
+let excuseWho = ["The dog", "My granma", "His turtle", "My bird"];
+let excuseWhat = ["eat", "pissed", "crushed", "broked"];
+let excuseWhen = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function sentence() {
+  var theSentence =
+    excuseWho[Math.floor(Math.random() * excuseWho.length)] +
+    " " +
+    excuseWhat[Math.floor(Math.random() * excuseWhat.length)] +
+    " " +
+    excuseWhen[Math.floor(Math.random() * excuseWhen.length)];
+  return theSentence;
+}
